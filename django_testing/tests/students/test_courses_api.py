2171c42e65c37aba04f30e3287c03f13442fe77b
Sergey28290 <77580966+Sergey28290@users.noreply.github.com>
2021-12-08 21:14:11 +0500

    Update test_courses_api.py

--- django_testing/tests/students/test_courses_api.py
+++ django_testing/tests/students/test_courses_api.py
@@ -5,9 +5,9 @@ from model_bakery import baker
 
 @pytest.mark.django_db #
 def test_first_courses():
-    customer = baker.make('students.Course') # создаем тестовую запись
-    queryset = Course.objects.all()[0] # получаем ее из бд
-    assert queryset == customer # проверяем на то, что это именно та запись
+    customer = baker.make('students.Course') 
+    queryset = Course.objects.all()[0] 
+    assert queryset == customer 
 
 
 @pytest.mark.django_db #
@@ -17,15 +17,15 @@ def test_list_courses():
         customer = baker.make('students.Course')
         customers.append(customer)
 
-    queryset = Course.objects.all() # получаем queryset, после мы его преобразовываем в список для сравнения
+    queryset = Course.objects.all() 
     assert list(queryset) == customers
 
 
 @pytest.mark.django_db
 def test_id_courses():
-    customer = baker.make('students.Course') # создаем одну запись
-    queryset = Course.objects.filter(id=customer.id) # получаем по id
-    assert list(queryset)[0] == customer # проверка первого, т.к. запись одна
+    customer = baker.make('students.Course')
+    queryset = Course.objects.filter(id=customer.id) 
+    assert list(queryset)[0] == customer
 
 @pytest.mark.django_db
 def test_name_courses():
@@ -35,21 +35,21 @@ def test_name_courses():
 
 @pytest.mark.django_db
 def test_create_courses():
-    user = Student.objects.create(name='Alex') # создаем пользователя
-    customer = Course.objects.create(name='stepone') # создаем курс
-    assert customer # проверяем что просто запустился
+    user = Student.objects.create(name='Alex') 
+    customer = Course.objects.create(name='stepone')
+    assert customer
 
 
 @pytest.mark.django_db
 def test_update_courses():
-    customer = baker.make('students.Course') # создаем запись
-    customer.name = 'Sergey' # меняем его значения
-    customer.save() # сохраняем
+    customer = baker.make('students.Course') 
+    customer.name = 'Sergey'
+    customer.save() 
     assert customer
 
 
 @pytest.mark.django_db
 def test_delete_courses():
-    customer = baker.make('students.Course') # создаем запись
-    customer.delete() # удаляем
+    customer = baker.make('students.Course') 
+    customer.delete()
     assert customer
